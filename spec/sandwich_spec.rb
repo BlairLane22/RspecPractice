73da870959ce80038de65406d241eeb5ffd4fe95
Blair <blairjacklane@gmail.com>
2017-12-06 21:45:05 -0500

    Adding more

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,10 +1,26 @@
+
 Sandwich = Struct.new(:taste, :toppings)
 
+
+# This is a Helper Method
+def sandwich
+  @sandwich ||= Sandwich.new('delicious', [])
+end
+
+
 RSpec.describe 'An ideal sandwich' do
+  # IDEA: (Hooks) before { @sandwich = Sandwich.new('delicious', []) }
+
   it "is delicious" do
-    sandwich = Sandwich.new('delicious', [])
     taste = sandwich.taste
 
     expect(taste).to eq('delicious')
+  end
+
+  it "lets me add toppings" do
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
   end
 end
